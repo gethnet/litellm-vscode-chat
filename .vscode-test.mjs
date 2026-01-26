a41b2cc2d4e498b9dd5b480b28611df332345ab3
amwdrizz <amwdrizz@users.noreply.github.com>
2026-01-26 16:55:42 +0000

    adding test coverage

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -5,6 +5,13 @@ export default defineConfig({
   mocha: {
     ui: 'tdd',
     timeout: 20000,
-    color: true
+    color: true,
+    reporter: process.env.VSCODE_TEST_RESULTS_DIR ? 'mocha-multi-reporters' : 'spec',
+    reporterOptions: process.env.VSCODE_TEST_RESULTS_DIR ? {
+      reporterEnabled: 'spec, mocha-junit-reporter',
+      mochaJunitReporterReporterOptions: {
+        mochaFile: `${process.env.VSCODE_TEST_RESULTS_DIR}/test-results.xml`
+      }
+    } : undefined
   }
 });
